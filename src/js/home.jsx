224f70d5704692fa811cb93cc3f9da292a5dc9f4
ssPLACH <134180679+ssPLACH@users.noreply.github.com>
2023-10-21 03:57:50 +0000

    primera plana

---
+++ src/js/home.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Card from "./card"
+
+
+//include images into your bundle
+
+//create your first component
+const Home = () => {
+	return (
+		<>
+			<div className="bg-success text-white">
+				<div className="text-center">
+					<h1 className="text-center mt-5">Let's Play Tic Tac Toe!</h1>
+					<h3 className="text-center mt-5">Pick A Weapon</h3>
+					<Card />
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
